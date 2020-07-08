11ee75716b7cd5d1ca2f439e193ef34b7eb8144b
Inna12018 <innadrabchuk@gmail.com>
2020-07-08 18:37:48 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function(){
 
+$('.header__menu-link').on('click',function(){
+    $(this).toggleClass('active');
+
+});
+
+$('.header__slider').slick({
+    infinite: true,
+    dots: true,
+
+});
 
 
   
